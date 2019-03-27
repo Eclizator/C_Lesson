7d5f5a863e771208d9e7e6a40f0c73b285a47b79
Dima <32442240+Eclizator@users.noreply.github.com>
2019-03-27 09:26:34 +0200

    Update task_00.cpp

--- task_00.cpp
+++ task_00.cpp
@@ -48,13 +48,13 @@ void task_00( int a) {
 	if ((a < 0) && (a >= -10)){
 			write(1, "The number is negative and less or equal to 10\n", 47);
 	}
-	else if ((a < -10) && (a >= -100)){
+	else if ((a > -10) && (a >= -100)){
 		write(1, "The number is negative and more than 10 and less or equal to 100\n", 61);
 	}
-	else if ((a < -100) && (a >= -1000)){
+	else if ((a > -100) && (a >= -1000)){
 		write(1, "The number is negative and more than 100 and less or equal to 1000\n", 68);
 	}
-	else if  (a < -1000) {
+	else if  (a > -1000) {
 		write(1, "The number is negative and more than 1000\n", 43);
 	}
 
