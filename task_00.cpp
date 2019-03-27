ff04db6e9784e1966eb50bb0523fe019913b6bfc
Dima <32442240+Eclizator@users.noreply.github.com>
2019-03-27 11:06:30 +0200

    Update task_00.cpp

--- task_00.cpp
+++ task_00.cpp
@@ -29,16 +29,16 @@ void task_00( int a) {
 	if (a == 0) { 
 		write(1, "The number is zero\n", 20);
 		}
-	else if ((a > 0) && (a >= 10)){
+	if ((a > 0) && (a >= 10)){
 		write(1, "The number is positive and less or equal to 10\n", 48);
 	}
-	else if ((a > 10) && (a <= 100)){
+	else ((a > 10) && (a <= 100)){
 		write(1, "The number is positive and more than 10 and less or equal to 100\n", 66);
 	}
-	else if ((a > 100) && (a <= 1000)){
+	if ((a > 100) && (a <= 1000)){
 		write(1, "The number is positive and more than 100 and less or equal to 1000\n", 68);
 	}
-	else if (a > 1000) {
+	else (a > 1000) {
 		write(1, "The number is positive and more than 1000\n", 43);
 	}
 
@@ -48,13 +48,13 @@ void task_00( int a) {
 	if ((a < 0) && (a <= -10)){
 		write(1, "The number is negative and less or equal to 10\n", 47);
 	}
-	else if ((a < -10) && (a <= -100)){
+	else ((a < -10) && (a <= -100)){
 		write(1, "The number is negative and more than 10 and less or equal to 100\n", 61);
 	}
-	else if ((a < -100) && (a <= -1000)){
+	if ((a < -100) && (a <= -1000)){
 		write(1, "The number is negative and more than 100 and less or equal to 1000\n", 68);
 	}
-	else if (a > -1000) {
+	else (a > -1000) {
 		write(1, "The number is negative and more than 1000\n", 43);
 	}
 
